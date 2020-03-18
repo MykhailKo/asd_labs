05975472e9855f6458b33438ae76335637818857
MykhailKo <tuareg1812@gmail.com>
2020-03-18 23:04:20 +0200

    Lab3 commit

---
+++ c#/Labs/lab3/Lab3Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MyArrayLab3;
+
+namespace Lab3Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] array = new int[3][];
+            array[0] = new[] {1, 44, 12, 6, 87};
+            array[1] = new[] {2, 43, 6, 7};
+            array[2] = new[] {34, 76};
+            
+            MyArray ma = new MyArray(array);
+            
+            Console.WriteLine(ma.Sum);
+            Console.WriteLine(ma[1]);
+            Console.WriteLine(ma[0]);
+        }
+    }
+}
---
+++ c#/Labs/lab3/MyArrayLab3/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace MyArrayLab3
+{
+    public class MyArray
+    {
+        private int _height;
+        private int[][] _matrixValues;
+        private int _elementsSum;
+
+        public int Sum
+        {
+            get
+            {
+                return _elementsSum;
+            }
+        }
+
+        public MyArray(int[][] array)
+        {
+            _matrixValues = array;
+            _height = array.Length;
+            for (int i = 0; i < _height; i++) {
+                for (int j = 0; j < _matrixValues[i].Length; j++) {
+                    _elementsSum += _matrixValues[i][j];
+                }
+            }
+        }
+
+        public MyArray()
+        {
+            _matrixValues = new int[][] {};
+            _height = 0;
+            _elementsSum = 0;
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                if (index >= _height || index < 0)
+                {
+                    return 0;
+                }
+                double sum = 0;
+                for (int i = 0; i < _matrixValues[index].Length; i++)
+                {
+                    sum += (int)Math.Pow(_matrixValues[index][i], 2);
+                }
+                sum /= _matrixValues[index].Length;
+                return Math.Sqrt(sum);
+            }
+        }
+    }
+}
